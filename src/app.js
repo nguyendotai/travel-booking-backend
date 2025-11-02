16970023eb9a9ba9246f85e70eb674df86a8e29d
nguyendotai <nguyendotai11@gmail.com>
2025-11-02 21:48:26 +0700

    update app.js listen

--- src/app.js
+++ src/app.js
@@ -43,8 +43,8 @@ app.use("/api/reviews", reviewRoutes);
 sequelize.authenticate()
   .then(() => {
     console.log("✅ Database connected");
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    const PORT = process.env.PORT || 5000; // Giữ nguyên dòng này
+    app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch((err) => console.error("❌ Database connection error:", err));
 
